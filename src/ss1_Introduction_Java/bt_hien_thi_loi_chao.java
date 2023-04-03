fcc7f4f709a925568e0e595013a79a3958a2fff9
PhanNguyenKha <phannguyenkha2409@gmail.com>
2023-04-03 11:09:11 +0700

    fc

---
+++ src/ss1_Introduction_Java/bt_hien_thi_loi_chao.java
@@ -0,0 +1,12 @@
+package ss1_Introduction_Java;
+
+import java.util.Scanner;
+
+public class bt_hien_thi_loi_chao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Mời Nhập Tên");
+        String name = sc.nextLine();
+        System.out.println("Hello" + name);
+    }
+}
---
+++ src/ss1_Introduction_Java/bt_ung_dung_chuyen_doi_tien_te.java
@@ -0,0 +1,7 @@
+package ss1_Introduction_Java;
+
+public class bt_ung_dung_chuyen_doi_tien_te {
+    public static void main(String[] args) {
+        
+    }
+}
