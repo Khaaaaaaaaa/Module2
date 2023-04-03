17416fffc463691668dcedf77bb7614a46a27570
PhanNguyenKha <phannguyenkha2409@gmail.com>
2023-04-03 14:42:31 +0700

    1

--- src/ss1_Introduction_Java/bt_ung_dung_chuyen_doi_tien_te.java
+++ src/ss1_Introduction_Java/bt_ung_dung_chuyen_doi_tien_te.java
@@ -1,7 +1,16 @@
 package ss1_Introduction_Java;
 
+import java.util.Scanner;
+
 public class bt_ung_dung_chuyen_doi_tien_te {
     public static void main(String[] args) {
-        
+        double vnd = 23000;
+        double usd;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Mời Bạn Nhập ");
+        usd = sc.nextDouble();
+        double quydoi = usd * 23000;
+        System.out.println("Giá Trị Hiện Tại" + quydoi);
+
     }
 }
