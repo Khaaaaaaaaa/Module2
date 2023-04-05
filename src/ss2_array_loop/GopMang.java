ec7f6ec7ea200e393a834c8623ba32be5cada825
PhanNguyenKha <phannguyenkha2409@gmail.com>
2023-04-05 10:30:44 +0700

    1

---
+++ src/ss2_array_loop/GopMang.java
@@ -0,0 +1,14 @@
+package ss2_array_loop;
+
+import java.util.Arrays;
+
+public class GopMang {
+    public static void main(String[] args) {
+        int [] array1= {1,2,3,4,5};
+        int [] array2= {6,7,8,9,10};
+        int [] array3= new int[array1.length+array2.length];
+        System.arraycopy(array1,0,array3,0,array1.length);
+        System.arraycopy(array2,0,array3,array1.length,array2.length);
+        System.out.println(Arrays.toString(array3));
+    }
+}
