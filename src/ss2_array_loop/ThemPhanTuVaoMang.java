0e9a6ffd740c7e3acde3a0abf5726793dea88aa2
PhanNguyenKha <phannguyenkha2409@gmail.com>
2023-04-05 10:21:46 +0700

    1

--- src/ss2_array_loop/ThemPhanTuVaoMang.java
+++ src/ss2_array_loop/ThemPhanTuVaoMang.java
@@ -2,21 +2,18 @@ package ss2_array_loop;
 
 import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ThemPhanTuVaoMang {
     public static void main(String[] args) {
-     Scanner sc = new Scanner(System.in);
-        System.out.println("Nhap Vao Do Dai Cua Mang");
-        int size = Integer.parseInt(sc.nextLine());
-        int [] number = new int [size];
-        for (int i = 0; i < number.length ; i++) {
-            System.out.println("Nhap Vao Phan Tu Thu" + (i+1));
-            number[i] = Integer.parseInt(sc.nextLine());
-            System.out.println(Arrays.toString(number));
-            System.out.println(number.toString());
-
-
-        }
+        String[] arr = {"1,2,3,4,5"};
+        System.out.println("Mảng Ban Đầu Là :" + Arrays.toString(arr));
+        int n = arr.length;
+        arr = Arrays.copyOf(arr, n + 1);
+        arr[n] = "6";
+        System.out.println("Mảng Mới Là:"
+                + Arrays.toString(arr));
     }
 }
+
